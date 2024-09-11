cfb1b97710e3d8e6bc5151f2b90982ab68506a82
AlexandraIonescuMiroiu <lyannisart@gmail.com>
2024-09-11 20:08:54 +0200

    2.1 hecho

--- Assets/Scripts/Ejercicio1_1.cs
+++ Assets/Scripts/Ejercicio1_1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,25 @@ using UnityEngine;
 public class Ejercicio1_1 : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    string texto = "¡Hola Mundo!";
+    string nombre = "Alexandra";
+    string frame = "Ha pasado un frame";
+
+
+
+
     void Start()
     {
-        
+       
+       Debug.Log(texto);
+       Debug.Log("Este es el primer videojuego de " + "nombre");
+       Debug.Log("Estoy aprendiendo C#");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(frame);
     }
 }
---
+++ Assets/Scripts/Ejercicio2_1.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Ejercicio2_1 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    int edad;
+    float altura;
+    string nombre;
+    float iva;
+    float peso;
+    bool alumnoRepetidor;
+    char letra;
+    float minutos;
+    string matriculaCoche;
+    bool mayorDeEdad;
+    int codigoPostal;
+    bool genero;
+    int numeroDeHijos;
+    float tallaCamisa;
+    float precio;
+    string mensaje;
+    bool mayorEdad;
+    int dias;
+    float contador;
+    char tallaCamiseta;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

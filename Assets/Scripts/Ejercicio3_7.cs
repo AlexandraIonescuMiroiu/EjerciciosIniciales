1941f1fb233ed3b74fd94419a07ae10c153dfa33
AlexandraIonescuMiroiu <lyannisart@gmail.com>
2024-09-16 16:50:45 +0200

    3.7

--- Assets/Scripts/Ejercicio3_7.cs
+++ Assets/Scripts/Ejercicio3_7.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Ejercicio3_7 : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] int vEnemigo;
+    [SerializeField] int aTriangulo;
+    [SerializeField] int resultado;
     void Start()
     {
-        
+        resultado= vEnemigo*aTriangulo;
+        Debug.Log(resultado);
     }
 
     // Update is called once per frame

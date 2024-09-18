0d059b2d91ecb279ea11edadf2c6ba9c7c53769a
AlexandraIonescuMiroiu <lyannisart@gmail.com>
2024-09-18 19:19:40 +0200

    4.5 yaa xdd tampoco se hacerlo

--- Assets/Scripts/Ejercicio4_4.cs
+++ Assets/Scripts/Ejercicio4_4.cs
@@ -7,12 +7,19 @@ public class Ejercicio4_4 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float velocidadConvertidaenMetrosPorSegundo = Conversor(90);
+        Debug.Log("90km/h convertidos en metros por segundos, son" + velocidadConvertidaenMetrosPorSegundo + " m/s");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    float Conversor(float velocidadKMporHora)
+    {
+        float velocidadConvertida = velocidadKMporHora * 1000f / 3600f;
+        return velocidadConvertida;
     }
 }

14123e8292673e5254b4f80890143a39794d30df
AlexandraIonescuMiroiu <lyannisart@gmail.com>
2024-09-18 19:01:48 +0200

    2.8 aah
    aa

--- Assets/Scripts/Ejercicio2_8.cs
+++ Assets/Scripts/Ejercicio2_8.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class Ejercicio2_8 : MonoBehaviour
 {
     // Start is called before the first frame update
-    //idk
+    float vida = 120f;
+    float pVida;
     void Start()
     {
-        
+        pVida = (vida * 3) / 100;
+
+        vida -= pVida;
+        Debug.Log(vida);
+
+        vida -= pVida * 2;
+        Debug.Log(vida); 
+
+        vida -= pVida * 3;
+        Debug.Log(vida);
+
+        vida -= pVida * 4;
+        Debug.Log(vida);
+
+        vida -= pVida * 5;
+        Debug.Log(vida);
+
     }
 
     // Update is called once per frame

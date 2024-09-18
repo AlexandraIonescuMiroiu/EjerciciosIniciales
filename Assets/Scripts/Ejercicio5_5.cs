3068f2f00b71fca606e86ac4b536c5ed2921dfdf
AlexandraIonescuMiroiu <lyannisart@gmail.com>
2024-09-18 21:11:20 +0200

    aa
    aa

--- Assets/Scripts/Ejercicio5_5.cs
+++ Assets/Scripts/Ejercicio5_5.cs
@@ -7,7 +7,7 @@ public class Ejercicio5_5 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        aaa
     }
 
     // Update is called once per frame
